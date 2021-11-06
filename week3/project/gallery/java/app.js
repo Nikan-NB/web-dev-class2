d173cae367da37d5385b5297d8294fe1f99cf9b9
Nikan <nikan.nb1@gmail.com>
2021-11-06 16:13:21 +0330

    week 3

--- week3/project/gallery/java/app.js
+++ week3/project/gallery/java/app.js
@@ -1,31 +1,56 @@
-const img1 =document.getElementById('1');
-const img2 =document.getElementById('2');
-const img3 =document.getElementById('3');
-const img4 =document.getElementById('4');
-const img5 =document.getElementById('5');
-const img6 =document.getElementById('6');
+
+// img1.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '1' + '.jpg';
+// })
+
+// img2.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '2' + '.jpg';
+// })
+
+// img3.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '3' + '.jpg';
+// })
+
+// img4.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '4' + '.jpg';
+// })
+
+// img5.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '5' + '.jpg';
+// })
+
+// img6.addEventListener('click' , function(){
+//     asli.style.display = 'block' 
+//     modal.src = 'image/' + '6' + '.jpg';
+// })
+
+// close.addEventListener('click' ,function(){
+//     asli.style.display = 'none' 
+// })
+// const img1 =document.getElementById('1');
+// const img2 =document.getElementById('2');
+// const img3 =document.getElementById('3');
+// const img4 =document.getElementById('4');
+// const img5 =document.getElementById('5');
+// const img6 =document.getElementById('6');
 const modal =document.getElementById('image');
+const asli =document.getElementById('modal');
+const close =document.getElementById('cls');
 
-img1.addEventListener('click' , function(){
-    modal.src = 'image/' + '1' + '.jpg';
+const images = document.getElementsByClassName('image');
+
+for (let i = 0 ; i < images.length ; i++){
+    images[i].addEventListener('click' ,function(e){
+    asli.style.display = 'block' 
+    modal.src = e.target.src;
 })
+}
 
-img2.addEventListener('click' , function(){
-    modal.src = 'image/' + '2' + '.jpg';
-})
-
-img3.addEventListener('click' , function(){
-    modal.src = 'image/' + '3' + '.jpg';
-})
-
-img4.addEventListener('click' , function(){
-    modal.src = 'image/' + '4' + '.jpg';
-})
-
-img5.addEventListener('click' , function(){
-    modal.src = 'image/' + '5' + '.jpg';
-})
-
-img6.addEventListener('click' , function(){
-    modal.src = 'image/' + '6' + '.jpg';
+close.addEventListener('click' ,function(){
+    asli.style.display = 'none'
 })
